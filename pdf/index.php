62e39fb038b9b4a03486d03cc6fb32af5c622689
Ricardo Arturo <peralta.mendoza.ricardo.arturo@gmail.com>
2025-06-22 15:16:25 -0600

    Vista previa del archivo PDF
    Muestra una vista previa centrada y con estilo de un archivo PDF incrustado en la página, permitiendo visualizar el documento directamente desde el navegador.

---
+++ pdf/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="styles.css" />
+  <title>Visor PDF</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 class="title">📄 Vista previa del documento</h1>
+    <object class="pdfview" type="application/pdf" data="primero.pdf"></object>
+  </div>
+</body>
+
+</html>
